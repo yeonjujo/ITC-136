a9c10a29ff3599ae516fc869bbbd3c6ad43699a6
yeonjujo <yeonju0419@naver.com>
2017-04-04 14:47:31 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,1 +1,6 @@
 #This is a commont
+pwd          # stands for 'Print Working Directory' and tells me where I am (WHERE)
+whoami       #Tells me which user I'm logged in as (WHO)
+Is           #Tells me what files are in the directory (WHAT)
+cd           #Allows me to change into other directories (NAVIGATION)
+date         # what time is it on the machine (WHEN)
