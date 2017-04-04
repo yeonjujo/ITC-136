29fa2b2e5bb794a49abe30e31432f25a4433d7ac
yeonjujo <yeonju0419@naver.com>
2017-04-04 15:14:33 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -4,3 +4,8 @@ whoami       #Tells me which user I'm logged in as (WHO)
 Is           #Tells me what files are in the directory (WHAT)
 cd           #Allows me to change into other directories (NAVIGATION)
 date         # what time is it on the machine (WHEN)
+
+/var/root cat example.sh
+
+#!/bin/sh
+echo "This is text... from a command.  yay."
